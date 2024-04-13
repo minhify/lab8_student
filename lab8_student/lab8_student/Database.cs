64671f6b9ceac7189e9802602a026c9814bef1b2
Mee18 <nguyenseoul18@gmail.com>
2024-04-13 14:50:47 +0700

    add class connect to database

---
+++ lab8_student/lab8_student/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab8_student
+{
+    class Database
+    {
+        public static SqlConnection connection;
+        public static Boolean OpenConnection()
+        {
+            try
+            {
+                connection = new SqlConnection("Server=MINHIFY\\MYB2014996;database=SinhVien;Integrated Security=true");
+                connection.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return false;
+            }
+
+            return true;
+
+        }
+        public static Boolean CloseConnection()
+        {
+            connection.Close();
+            return true;
+        }
+        
+    }
+  
+}
